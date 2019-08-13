2005fe8c58239421fa73089c9e5caa7c8e3a15d7
gfike19@gmail.com <Vaughn*573>
2019-08-13 13:42:57 -0500

    adding files

---
+++ IT213_Unit1/IT213_Fike_Unit1_Exercise1.js
@@ -0,0 +1,5 @@
+var number1 = 4;
+var number2 = 5;
+var sum;
+// print("")
+console.log("The sum of number1 and number2 is " + (number1 + number2));
---
+++ IT213_Unit1/IT213_Fike_Unit1_Exercise2.js
@@ -0,0 +1,5 @@
+var String = "20";
+var number = Number(String);
+var number2 = 10;
+//print(“the variable number divided by number2 is equal to X”, )
+console.log("the variable number divided by number2 is equal to " + (number / number2));
